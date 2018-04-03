73c87c815b532c0ba506b6c4961a1bc0828189e4
kendric <khood5@kent.edu>
2018-04-02 22:10:25 -0400

    Cleaned up part 1 of project one

---
+++ DroneProjectONE/MAVLinkCommands.py
@@ -0,0 +1,217 @@
+#Kendric Hood, Betis Baheri, Emil Shirima, Govinda Baweja
+
+#This is a set of functions that run the basic MAVLink commands
+#Anyone can add to this or edit the functions as they see fit.
+
+import time
+from dronekit import connect, VehicleMode
+from pymavlink import mavutil
+
+def mav_steup():
+    # Set up option parsing to get connection string
+    import argparse
+    parser = argparse.ArgumentParser(description='Commands vehicle using vehicle.simple_goto.')
+    parser.add_argument('--connect',
+                        help="Vehicle connection target string. If not specified, SITL automatically started and used.")
+    args = parser.parse_args()
+
+    connection_string = args.connect
+    global sitl
+    sitl= None
+
+    # Start SITL if no connection string specified
+    if not connection_string:
+        import dronekit_sitl
+        sitl = dronekit_sitl.start_default()
+        connection_string = sitl.connection_string()
+
+    # Connect to the Vehicle
+    print('Connecting to vehicle on: %s' % connection_string)
+    global vehicle
+    vehicle = connect(connection_string, wait_ready=True)
+
+def mav_cleanup():
+    # Close vehicle object before exiting script
+    print("Close vehicle object")
+    global vehicle
+    vehicle.close()
+
+    # Shut down simulator if it was started.
+    global sitl
+    if sitl:
+        sitl.stop()
+
+def return_to_launch():
+    print("Landing at launch location")
+    msg = vehicle.message_factory.command_long_encode(
+        0,  # Target system
+        0,  # Target comtroler
+        mavutil.mavlink.MAV_CMD_NAV_RETURN_TO_LAUNCH,  # command
+        0,  # frame
+        0,  # param 1
+        0,  # param 2
+        0,  # param 3
+        0,  # param 4
+        0,  # param 5
+        0,  # param 6
+        0   # param 7
+    )
+    vehicle.send_mavlink(msg)
+    vehicle.flush()
+
+    while(vehicle.location.global_relative_frame.alt > 0):
+        if vehicle.location.global_relative_frame.alt == 0:
+            print("Landed")
+            break
+        else:
+            print("Lat:", vehicle.location.global_relative_frame.lat, ", Lon:",vehicle.location.global_relative_frame.lon, ", Alt:", vehicle.location.global_relative_frame.alt)
+            time.sleep(1)
+    print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+    time.sleep(5)
+
+def arm_and_takeoff(aTargetAltitude):
+    print("Basic pre-arm checks")
+    # Don't try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+
+    time.sleep(2)
+    print("Arming motors")
+    # Copter should arm in GUIDED mode
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        time.sleep(1)
+
+    print("Taking off!")
+    msg = vehicle.message_factory.command_long_encode(
+        0,#Target system
+        0,#Target comtroler
+        mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,#command
+        0,#frame
+        0,#param 1
+        0,#param 2
+        0,#param 3
+        0,#param 4
+        0,#param 5
+        0,#param 6
+        aTargetAltitude#param 7
+    )
+    vehicle.send_mavlink(msg)
+    vehicle.flush()
+    while(vehicle.location.global_relative_frame.alt < aTargetAltitude):
+        if vehicle.location.global_relative_frame.alt >= aTargetAltitude:
+            print("Reached target altitude")
+            break
+        else:
+            print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+            time.sleep(1)
+    print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+    time.sleep(5)
+
+def land_here():
+    print("Setting LAND mode")
+    vehicle.mode = VehicleMode("LAND")
+
+    while (vehicle.location.global_relative_frame.alt > 0):
+        if vehicle.location.global_relative_frame.alt == 0:
+            print("Landed")
+            break
+        else:
+            print(
+            "Lat:", vehicle.location.global_relative_frame.lat, ", Lon:", vehicle.location.global_relative_frame.lon,
+            ", Alt:", vehicle.location.global_relative_frame.alt)
+            time.sleep(1)
+    print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+    time.sleep(5)
+
+def local_goto(lat,lon,alt):
+    print("going to ",lat,", ",lon)
+    vehicle._master.mav.mission_item_send(
+        0,# Target system
+        0,# Target comtroler
+        0,# Waypoint number
+        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,  # frame
+        mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,# command
+        2,# current waypoint number
+        0,# auto countue to next waypoint
+        0,# param 1
+        0,# param 2
+        0,# param 3
+        0,# param 4
+        lat,# param 5
+        lon,# param 6
+        alt# param 7
+    )
+    while((vehicle.location.global_relative_frame.lat > lat + 0.0000009 or vehicle.location.global_relative_frame.lat < lat - 0.0000009) and (vehicle.location.global_relative_frame.lon > lon + 0.0000009 or vehicle.location.global_relative_frame.lon < lon- 0.0000009)):
+        print("Lat:",vehicle.location.global_relative_frame.lat,", Lon:",vehicle.location.global_relative_frame.lon, ", Alt:",vehicle.location.global_relative_frame.alt)
+        time.sleep(1)
+
+    print("Lat:", vehicle.location.global_relative_frame.lat, ", Lon:", vehicle.location.global_relative_frame.lon,", Alt:", vehicle.location.global_relative_frame.alt)
+    time.sleep(5)
+
+def global_goto(lat,lon,alt):
+    print("going to ", lat, ", ", lon)
+    vehicle._master.mav.mission_item_send(
+        0,  # Target system
+        0,  # Target comtroler
+        0,  # Waypoint number
+        mavutil.mavlink.MAV_FRAME_GLOBAL,  # frame
+        mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,  # command
+        2,  # current waypoint number
+        0,  # auto countue to next waypoint
+        0,  # param 1
+        0,  # param 2
+        0,  # param 3
+        0,  # param 4
+        lat,  # param 5
+        lon,  # param 6
+        alt  # param 7
+    )
+    while ((vehicle.location.global_relative_frame.lat > lat + 0.0000009 or vehicle.location.global_relative_frame.lat < lat) and (vehicle.location.global_relative_frame.lon > lon + 0.0000009 or vehicle.location.global_relative_frame.lon < lon)):
+        print("Lat:", vehicle.location.global_relative_frame.lat, ", Lon:", vehicle.location.global_relative_frame.lon,", Alt:", vehicle.location.global_relative_frame.alt)
+        time.sleep(1)
+
+    print("Lat:", vehicle.location.global_relative_frame.lat, ", Lon:", vehicle.location.global_relative_frame.lon,
+          ", Alt:", vehicle.location.global_relative_frame.alt)
+    time.sleep(5)
+
+def set_groundspeed(speed):
+    msg = vehicle.message_factory.command_long_encode(
+        0,# target system
+        0,# target component
+        mavutil.mavlink.MAV_CMD_DO_CHANGE_SPEED, # command
+        0, # confirmation
+        1, # param 1 1=groundspeed
+        speed, # param 2 speed in metres/second
+        0,# param 3
+        0,# param 4
+        0,# param 5
+        0,# param 6
+        0 # param 7
+        )
+
+    # send command to vehicle
+    vehicle.send_mavlink(msg)
+    vehicle.flush()
+
+def set_airspeed(speed):
+    msg = vehicle.message_factory.command_long_encode(
+        0,# target system
+        0,# target component
+        mavutil.mavlink.MAV_CMD_DO_CHANGE_SPEED, # command
+        0, # confirmation
+        0, # param 1 0=airspeed
+        speed, # param 2 speed in metres/second
+        0,# param 3
+        0,# param 4
+        0,# param 5
+        0,# param 6
+        0 # param 7
+        )
+
+    # send command to vehicle
+    vehicle.send_mavlink(msg)
+    vehicle.flush()
--- DroneProjectONE/Part_ONE.py
+++ DroneProjectONE/Part_ONE.py
@@ -1,120 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+#Kendric Hood, Betis Baheri, Emil Shirima, Govinda Baweja
 
 from __future__ import print_function
-import time
-from dronekit import connect, VehicleMode, LocationGlobalRelative , Command , LocationGlobal
-from pymavlink import mavutil
-import math
-import subprocess
-# Set up option parsing to get connection string
-import argparse
-parser = argparse.ArgumentParser(description='Commands vehicle using vehicle.simple_goto.')
-parser.add_argument('--connect',
-                    help="Vehicle connection target string. If not specified, SITL automatically started and used.")
-args = parser.parse_args()
+from MAVLinkCommands import *
 
-connection_string = args.connect
-sitl = None
-
-
-# Start SITL if no connection string specified
-if not connection_string:
-    import dronekit_sitl
-    sitl = dronekit_sitl.start_default()
-    connection_string = sitl.connection_string()
-
-
-# Connect to the Vehicle
-print('Connecting to vehicle on: %s' % connection_string)
-vehicle = connect(connection_string, wait_ready=True)
-print("Basic pre-arm checks")
-# Don't try to arm until autopilot is ready
-while not vehicle.is_armable:
-    print(" Waiting for vehicle to initialise...")
-    time.sleep(2)
-
-print("Arming motors")
-# Copter should arm in GUIDED mode
-vehicle.mode = VehicleMode("GUIDED")
-vehicle.armed = True
-# Confirm vehicle armed before attempting to take off
-while not vehicle.armed:
-    print(" Waiting for arming...")
-    time.sleep(2)
-print("Taking off!")
-altitude = float(0.5)
-if math.isnan(altitude) or math.isinf(altitude):
-    raise ValueError("Altitude was NaN or Infinity. Please provide a real number")
-vehicle._master.mav.command_long_send(0, 0, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,
-0, 0, 0, 0, 0, 0, 0, altitude)
-while True:
-    print(" Altitude: ", vehicle.location.global_relative_frame.alt)
-    # Break and return from function just below target altitude.
-    if vehicle.location.global_relative_frame.alt >= altitude * 0.95:
-        print("Reached target altitude")
-        break
-    time.sleep(8)
-# msg = vehicle.message_factory.command_long_encode(
-#         0, 0,    # target system, target component
-#         mavutil.mavlink.MAV_CMD_DO_SET_HOME, #command
-#         0, #confirmation
-#         1, #param 1: 1 to use current position, 2 to use the entered values.
-#             0, 0, 0, #params 2-4
-#         vehicle.location.lat,
-#         vehicle.location.lon,
-#         vehicle.location.alt
-#         )
 lat=-35.361879
 lon=149.166033
-frame = mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT
-alt = 30
-vehicle._master.mav.mission_item_send(0, 0, 0, frame,
-                                           mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 2, 0, 0,
-                                           0, 0, 0, lat, lon,
-                                           alt)
-speed_type = 0 # air speed
-msg = vehicle.message_factory.command_long_encode(
-    0, 0,    # target system, target component
-    mavutil.mavlink.MAV_CMD_DO_CHANGE_SPEED, #command
-    0, #confirmation
-    speed_type, #param 1
-    5, # speed in metres/second
-    -1, 0, 0, 0, 0 #param 3 - 7
-    )
-vehicle.send_mavlink(msg)
-time.sleep(2)
-# cmds = vehicle.commands
-# cmds.download()
-# cmds.wait_ready()
-# cmds = vehicle.commands
-# cmds.clear()
-# cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_LOITER_TIME,
-#     25, 0, 0, 0, 0, 0,
-#     0, 0, 0)
-# cmds.add(cmd)
-# cmds.upload()
-msg = vehicle.message_factory.command_long_encode(
-    0, 0,    # target system, target component
-    mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
-    mavutil.mavlink.MAV_CMD_NAV_LOITER_TIME,
-    10, #param 1
-    0,
-    0, 0, 0, 0, 0
-    )
-vehicle.send_mavlink(msg)
-time.sleep(2)
-time.sleep(65)
+alt=10
+speed=10
 
-vehicle._master.mav.command_long_send(0, 0,
-                    mavutil.mavlink.MAV_CMD_NAV_RETURN_TO_LAUNCH, 0, 0, 0, 0, 0, 0, 0, 1)
-print("Setting LAND mode...")
-vehicle.mode = VehicleMode("LAND")
-time.sleep(50)
-print("Close vehicle object")
-vehicle.close()
-
-# Shut down simulator if it was started.
-if sitl:
-    sitl.stop()
+mav_steup()
+arm_and_takeoff(alt)
+set_airspeed(speed)
+local_goto(lat,lon,alt)
+land_here()
+mav_cleanup()
